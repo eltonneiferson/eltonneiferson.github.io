2f90be4b6b35a18079fa7da738aa348f7c55589b
elton <eltonneiferson@outlook.com>
2022-11-15 20:07:47 -0300

    Update darkMode

--- main.js
+++ main.js
@@ -1,39 +1,19 @@
 const main = document.querySelector('#container')
-const networks = document.querySelector('.networks')
-const aboutMe =  document.querySelector('.about-me')
+const spans = document.querySelectorAll('span')
+const links = document.querySelectorAll('.link')
 const button = document.querySelector('.on-off')
 
-const userName = 'Elton Neiferson'
-const linkedin = 'https://www.linkedin.com/in/elton-neiferson-134431213/'
-const gitHub = 'https://github.com/eltonneiferson/'
-const instagram = 'https://www.instagram.com/eltonneiferson/'
-const email = 'eltonneiferson@outlook.com'
-const skills = 'html,css,js,git,'
-
-function darkMode() {
+button.addEventListener('click', () => {
     button.classList.toggle('moveButton')
     main.classList.toggle('dark-mode')
     main.classList.toggle('white-mode')
-}
+    
+    spans.forEach((span) => span.classList.toggle('spanWhiteMode'))
 
-aboutMe.innerHTML = `
-    <img class="img-elton" src="https://avatars.githubusercontent.com/u/85045713?v=4" alt="Foto de Elton">    
-    <h1>${userName}</h1>
-    <h2 class="function-dev">fullStackDeveloper<span>()</span></h2>
-    <img class="icons-dev" src="https://skillicons.dev/icons?i=${skills}"/>
-`
-networks.innerHTML = `
-    <a href="${linkedin}" target="_blank">
-        <img class="network-icon" src="https://skillicons.dev/icons?i=linkedin"/>
-    </a>
-    <a href="${gitHub}" target="_blank">
-        <img class="network-icon" src="https://skillicons.dev/icons?i=github"/>
-    </a>
-    <a href="${instagram}" target="_blank">
-        <img class="network-icon" src="https://skillicons.dev/icons?i=instagram"/>
-    </a>
-    <a href="mailto:${email}" target="_blank">
-        <img class="network-icon" src="img/icon-outlook.ico"/>
-    </a>
-`
-button.addEventListener('click', darkMode)
+    links.forEach((link) => {
+        link.classList.toggle('link-darkMode')
+        link.classList.toggle('link-whiteMode')
+    })
+})
+
+console.log(spans)
