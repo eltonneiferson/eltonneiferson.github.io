1e6dc49d155b5f71d054f52b2ee91b75e0d12988
elton <eltonneiferson@outlook.com>
2022-11-15 18:35:55 -0300

    added darkMode

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
+const main = document.querySelector('#container')
 const networks = document.querySelector('.networks')
-const  typingName =  document.querySelector('.text')
+const aboutMe =  document.querySelector('.about-me')
+const button = document.querySelector('.on-off')
 
 const userName = 'Elton Neiferson'
 const linkedin = 'https://www.linkedin.com/in/elton-neiferson-134431213/'
 const gitHub = 'https://github.com/eltonneiferson/'
 const instagram = 'https://www.instagram.com/eltonneiferson/'
 const email = 'eltonneiferson@outlook.com'
-const skills = 'html,css,js,git'
+const skills = 'html,css,js,git,'
 
-typingName.innerHTML = `
+function darkMode() {
+    button.classList.toggle('moveButton')
+    main.classList.toggle('dark-mode')
+    main.classList.toggle('white-mode')
+}
+
+aboutMe.innerHTML = `
+    <img class="img-elton" src="https://avatars.githubusercontent.com/u/85045713?v=4" alt="Foto de Elton">    
     <h1>${userName}</h1>
-    <p class="function-dev">fullStackDeveloper<span>(){</span></p>
+    <h2 class="function-dev">fullStackDeveloper<span>()</span></h2>
     <img class="icons-dev" src="https://skillicons.dev/icons?i=${skills}"/>
-    <p style="text-align: left;">}</p>
 `
-
 networks.innerHTML = `
     <a href="${linkedin}" target="_blank">
         <img class="network-icon" src="https://skillicons.dev/icons?i=linkedin"/>
@@ -29,3 +36,4 @@ networks.innerHTML = `
         <img class="network-icon" src="img/icon-outlook.ico"/>
     </a>
 `
+button.addEventListener('click', darkMode)
